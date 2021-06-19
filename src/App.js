b3e76c1668ff7bf62b11bed097b15de089c594e6
shambu <shambuqwerty@gmail.com>
2021-06-19 15:19:54 +0530

    Components Todo, TodoList, TodoForm Done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import './App.css';
+import React from "react";
+import "./App.css";
+// import TodoForm from "./components/TodoForm";
+import TodoList from "./components/TodoList";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>To Do app</h1>
-    </div>
-  );
+	return (
+		<div className="todo-app">
+			<TodoList />
+		</div>
+	);
 }
 
 export default App;
