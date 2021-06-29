acf9084f96173874c2e61e4de81e86bb2a7b6319
shambu <shambuqwerty@gmail.com>
2021-06-29 14:28:53 +0530

    Resolved State change and implemented New Todo add button

--- src/components/Controller.js
+++ src/components/Controller.js
@@ -4,14 +4,13 @@ import TodoList from "./TodoList";
 
 function Controller() {
 	const [todos, setTodos] = useState([
-		"Read OS 20 pages",
-		"Read CN 20 pages",
-		"Solve 2 CP problems",
 		"Learn React",
-		"Gate preparation",
 		"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque pharetra. orem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque phar",
 		"Test Test Test... ",
 		"Test2 Test2 Test2.....",
+		"Hola, Amigos",
+		"With great power comes great responsibility",
+		"Lost in translation"
 	]);
 
 	const todo_submit = (form) => {
